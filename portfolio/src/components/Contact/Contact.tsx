0a96cdbd65b6d90d73bf6981a1892a28db5e5887
icecoil <jaz.ot.rok@gmail.com>
2024-04-11 20:56:36 +0200

    added content & styled Contact

--- portfolio/src/components/Contact/Contact.tsx
+++ portfolio/src/components/Contact/Contact.tsx
@@ -1,9 +1,41 @@
 import React from "react";
+import "./Contact.css";
+import eupass from "../../portfolio_pics/euPassIcon.png";
+import linkedIn from "../../portfolio_pics/linkedinIcon.png";
+import gitHub from "../../portfolio_pics/gitIcon.png";
+import fb from "../../portfolio_pics/fbIcon.png";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact">
       <h1>Contact</h1>
+      <div>
+        <p>Rok Krajnc</p>
+        <p>Knafelčeva ulica 45</p>
+        <p>2000 Lovrenc na Pohorju</p>
+        <p>Slovenija</p>
+      </div>
+      <div>
+        <p>Email: rok.krajnc@mail.com</p>
+        <p>Tel.: +38670675385</p>
+      </div>
+      <div className="contactLinks">
+        <a className="euPass">
+          <img src={eupass} alt="Europass" />
+        </a>
+        <a
+          target="_blanc"
+          href="https://www.linkedin.com/in/rok-krajnc-177a4420b/"
+        >
+          <img src={linkedIn} alt="Linkedin" />
+        </a>
+        <a target="_blanc" href="https://github.com/icecoil">
+          <img src={gitHub} alt="Github" />
+        </a>
+        <a target="_blanc" href="https://www.facebook.com/rok.krajnc">
+          <img src={fb} alt="Facebook" />
+        </a>
+      </div>
     </div>
   );
 };
